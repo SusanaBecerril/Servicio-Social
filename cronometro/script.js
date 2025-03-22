7fd321174a409441ea197f8eeb4ffd167674fe9f
Susana <202726090+SusanaBecerril@users.noreply.github.com>
2025-03-21 20:37:54 -0600

    proyecto del cronometro

---
+++ cronometro/script.js
@@ -0,0 +1,51 @@
+const stopwatch = document.getElementById('stopwatch');/*constantes*/
+const playPauseButton = document.getElementById('play-pause');
+const secondsSphere = document.getElementById('seconds-sphere');
+
+let stopwatchInterval;
+let runningTime = 0;/*segundos */
+
+const playPause = () => {/* si no esta pausado*/
+    const isPaused = !playPauseButton.classList.contains('running');
+    if (isPaused) {
+        playPauseButton.classList.add('running');
+        start();
+    } else {
+        playPauseButton.classList.remove('running');
+        pause();
+    }
+}
+
+const pause = () => {
+    secondsSphere.style.animationPlayState = 'paused';
+    clearInterval(stopwatchInterval);
+}
+
+const stop = () => {/*frena el tiempo*/
+    secondsSphere.style.transform = 'rotate(-90deg) translateX(60px)';/*grados*/
+    secondsSphere.style.animation = 'none';
+    playPauseButton.classList.remove('running');
+    runningTime = 0;
+    clearInterval(stopwatchInterval);
+    stopwatch.textContent = '00:00';
+}
+
+const start = () => {/*animacion de los segundos*/
+    secondsSphere.style.animation = 'rotacion 60s linear infinite';
+    let startTime = Date.now() - runningTime;
+    secondsSphere.style.animationPlayState = 'running';
+    stopwatchInterval = setInterval( () => {
+        runningTime = Date.now() - startTime;
+        stopwatch.textContent = calculateTime(runningTime);
+    }, 1000)
+}
+
+const calculateTime = runningTime => {/*calcula los segundos*/
+    const total_seconds = Math.floor(runningTime / 1000);/*total de segundos*/
+    const total_minutes = Math.floor(total_seconds / 60);/*total de minutos*/
+
+    const display_seconds = (total_seconds % 60).toString().padStart(2, "0");
+    const display_minutes = total_minutes.toString().padStart(2, "0");
+
+    return `${display_minutes}:${display_seconds}`
+}
