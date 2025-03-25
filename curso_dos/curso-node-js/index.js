72efd10e2fc0b4cb502b9714911a7707dce45b0f
Susana <202726090+SusanaBecerril@users.noreply.github.com>
2025-03-24 21:48:58 -0600

    CURSO DE NODE.JS DESDE CERO

---
+++ curso_dos/curso-node-js/index.js
@@ -0,0 +1,4 @@
+//CommonJS requiere module
+/*pratron de diseño*/
+const sum = require('./sum');  // Asegúrate de poner la ruta correcta
+console.log(sum(1, 2));
---
+++ curso_dos/curso-node-js/sum.js
@@ -0,0 +1,8 @@
+
+/*pratron de diseño*/
+/*exportar*/
+function sum(a, b) {
+    return a + b;
+}
+
+module.exports = sum;
